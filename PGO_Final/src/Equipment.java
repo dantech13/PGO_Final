0fae7ba61d66ca9bf1383283523ab16163655609
dantech13 <daniil.mur2004@gmail.com>
2026-06-16 22:25:20 +0200

    Rozbudowanie klas Student, Equipment oraz wstępnie main

--- PGO_Final/src/Equipment.java
+++ PGO_Final/src/Equipment.java
@@ -1,4 +1,37 @@
 public abstract class Equipment implements Displayable {
+
+    private String id;
+    private String name;
+    private double baseDailyPrice;
+    private boolean available;
+
+    public Equipment(String id, String name, double baseDailyPrice) {
+        this.id = id;
+        this.name = name;
+        this.baseDailyPrice = baseDailyPrice;
+        this.available = true;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBaseDailyPrice() {
+        return baseDailyPrice;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
     public abstract double calculateDailyPrice();
     public abstract String getDetails();
 }
